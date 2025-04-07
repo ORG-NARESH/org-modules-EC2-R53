6ebc7b8fb0f98e42cc12d44e3182ab55839212a4
naresh <Naresh@gmail.com>
2025-04-07 19:15:51 +0530

    org module

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "main" {
+  
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.instance_type
+
+  tags = {
+    Name = var.Name
+  }
+}
+
+resource "aws_route53_record" "main" {
+  
+  zone_id  = var.zone_id
+  name     = "${var.Name}-${var.env}.${var.domain}"
+  type     = "A"
+  ttl      = "10"
+  records  = [aws_instance.main.private_ip]
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "ami" {}
+variable "instance_type" {}
+variable "vpc_security_group_ids" {}
+variable "Name" {}
+variable "zone_id" {}
+variable "env" {}
+variable "domain" {}
