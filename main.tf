13438714c44a67cb55b725ffa43346be4e85b49d
naresh <Naresh@gmail.com>
2025-04-09 20:00:31 +0530

    created module

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "main" {
   
   ami                    = var.ami
   instance_type          = var.instance_type
-  vpc_security_group_ids = [var.vpc_security_group_ids]
+  vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
     Name = "${var.Name}-${var.env}"
