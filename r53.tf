7fc1a172cbc08271d16d47d54d33c8c4e9df499f
naresh <Naresh@gmail.com>
2025-04-14 12:54:42 +0530

    terraform expense config

--- r53.tf
+++ r53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "main" {
   
-  zone_id  = data.aws_route53_zone.main.zone_id
+  zone_id  = data.aws_route53_zone.main.id
   name     = "${var.Name}-${var.env}.${var.domain}"
   type     = "A"
   ttl      = "10"
