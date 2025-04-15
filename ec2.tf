a6b79a7a59f7744b30dbb077b5443b8b5bc27d60
naresh <Naresh@gmail.com>
2025-04-15 19:56:50 +0530

    terraform expense config

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "main" {
   
   ami                    = data.aws_ami.main.id 
   instance_type          = var.instance_type
-  vpc_security_group_ids = [var.vpc_security_group_ids] 
+  vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
     Name = "${var.Name}-${var.env}"
