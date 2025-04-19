4357dfb14db1c6bd8ad717792d7942636439f555
naresh <Naresh@gmail.com>
2025-04-19 12:45:02 +0530

    jenkins config

--- data.tf
+++ data.tf
@@ -30,7 +30,7 @@ data "aws_ami" "main" {
 
 
 data "aws_route53_zone" "main" {
-  name       = "eternallearnings.shop"
+  name       = var.domain
                           }
 
 # data "aws_route53_zone" "main" {
